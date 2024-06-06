dbc97cc2fbc69a2dd6805d910fe8db956b171544
Gaurang Khator <114048615+Gaurang-Khator@users.noreply.github.com>
2024-06-06 22:35:03 +0530

    Time: 87 ms (74.24%), Space: 73.1 MB (63.06%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -3,10 +3,11 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_map<int, int> mp;
         for(auto i: nums) {
-            if(mp[i] >= 1) {
+            mp[i]++;
+            if(mp[i] > 1) {
                 return true;
             }
-            mp[i]++;
+            
         }
         return false;
     }
