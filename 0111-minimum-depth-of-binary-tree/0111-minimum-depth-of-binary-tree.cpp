6b58f9278f9aa1558c615488bab31474eceb48cb
Gaurang Khator <114048615+Gaurang-Khator@users.noreply.github.com>
2024-06-17 10:59:40 +0530

    Time: 204 ms (12.05%), Space: 144.7 MB (64.24%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -18,7 +18,7 @@ public:
         int l = minDepth(root->left);
         int r = minDepth(root->right);
         if(l == 0 || r == 0) {
-            return l+r+1;
+            return max(l, r)+1;
         }
         return min(l, r)+1;
     }
