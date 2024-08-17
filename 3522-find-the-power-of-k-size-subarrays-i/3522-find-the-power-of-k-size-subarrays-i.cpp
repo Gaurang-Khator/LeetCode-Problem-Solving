bf1c80147ff0d243a7d35efb90989d10442ed1ba
Gaurang Khator <114048615+Gaurang-Khator@users.noreply.github.com>
2024-08-17 22:19:03 +0530

    Time: 21 ms (100%), Space: 33.2 MB (21.43%) - LeetHub

--- 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.cpp
+++ 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.cpp
@@ -2,13 +2,6 @@ class Solution {
 public:
     vector<int> resultsArray(vector<int>& nums, int k) {
         int n = nums.size();
-        if(n == k) {
-            if(is_sorted(nums.begin(), nums.end())) {
-                return {*max_element(nums.begin(), nums.end())};
-            } else {
-                return {-1};
-            }
-        }
         if(k == 1) {
             return nums;
         }
