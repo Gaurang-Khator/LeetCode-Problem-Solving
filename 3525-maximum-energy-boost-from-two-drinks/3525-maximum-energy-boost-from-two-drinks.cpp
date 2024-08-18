f453468f2c8ec30973681de948ad0732f8f8056c
Gaurang Khator <114048615+Gaurang-Khator@users.noreply.github.com>
2024-08-18 08:23:32 +0530

    Time: 212 ms (63.64%), Space: 195.5 MB (100%) - LeetHub

--- 3525-maximum-energy-boost-from-two-drinks/3525-maximum-energy-boost-from-two-drinks.cpp
+++ 3525-maximum-energy-boost-from-two-drinks/3525-maximum-energy-boost-from-two-drinks.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
     long long maxEnergyBoost(vector<int>& energyDrinkA, vector<int>& energyDrinkB) {
-        // long long res = LONG_LONG_MIN;
-        // int n = energyDrinkA.size();
-        // for(int i=0; i<n-1; i++) {
-        //     if(energyDrinkA[i] == energyDrinkB[i] || energyDrinkA[i+1] < energyDrinkB[i+1]) {
-        //         res--;
-        //     }
-        //     long long m = max(energyDrinkA[i], energyDrinkB[i]);
-        //     res += m;
-        // }
-        // return res;
+
         int n = energyDrinkA.size();
         
-        long long maxA = energyDrinkA[0]; // Start by drinking A
-        long long maxB = energyDrinkB[0]; // Start by drinking B
+        long long maxA = energyDrinkA[0]; 
+        long long maxB = energyDrinkB[0]; 
         
         for (int i = 1; i < n; i++) {
             long long newMaxA = max(maxA + energyDrinkA[i], maxB);
