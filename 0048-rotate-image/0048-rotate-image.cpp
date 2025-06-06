1993a0798c1d21d53434c1b3762e77c3b42d30c6
Gaurang Khator <114048615+Gaurang-Khator@users.noreply.github.com>
2025-06-06 23:22:53 +0530

    Time: 0 ms (100%), Space: 10.1 MB (68.53%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -3,18 +3,12 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         //transpose
         for(int i=0; i<matrix.size(); i++) {
-            for(int j=i+1; j<matrix.size(); j++) {
+            for(int j=i; j<matrix[0].size(); j++) {
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
-        //reverse rows
         for(int i=0; i<matrix.size(); i++) {
-            int x = 0, y = matrix[0].size()-1;
-            while(x < y) {
-                swap(matrix[i][x], matrix[i][y]);
-                x++;
-                y--;
-            }
+            reverse(matrix[i].begin(), matrix[i].end());
         }
     }
 };
