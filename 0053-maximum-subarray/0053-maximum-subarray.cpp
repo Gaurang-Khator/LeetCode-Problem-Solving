835daea4d2c8f1a3ccc2e4eef82c5c411a776413
Gaurang Khator <114048615+Gaurang-Khator@users.noreply.github.com>
2025-06-06 23:06:26 +0530

    Time: 0 ms (100%), Space: 71.7 MB (52.38%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,36 +1,16 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        //BRUTE FORCE
-
-        // int res = INT_MIN;
-        // for(int i=0; i<nums.size(); i++) {
-        //     int curr = 0;
-        //     for(int j=i; j<nums.size(); j++) {
-        //         curr += nums[j];
-        //         res = max(res, curr);
-        //     }
-        // }
-        // return res;
-
-        //KADANE'S ALGO
-
-        // int res = INT_MIN, sum = 0;
-        // for(int i=0; i<nums.size(); i++) {
-        //     sum += nums[i];
-        //     res = max(res, sum);
-        //     if(sum < 0) {
-        //         sum = 0;
-        //     }
-        // }
-        // return res;
-
-        int res = INT_MIN, curr = 0;
+        int sum = 0;
+        int maxSum = INT_MIN;
         for(int i=0; i<nums.size(); i++) {
-            curr += nums[i];
-            curr = max(curr, nums[i]);
-            res = max(curr, res);
+            sum += nums[i];
+            maxSum = max(maxSum, sum);
+
+            if(sum < 0){
+                sum = 0;
+            }
         }
-        return res;
+        return maxSum;
     }
 };
