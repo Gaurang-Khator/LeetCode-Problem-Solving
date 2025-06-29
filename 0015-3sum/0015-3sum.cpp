87b61354cb2a9ff744b3d1ebc9e5383df5935ced
Gaurang Khator <114048615+Gaurang-Khator@users.noreply.github.com>
2025-06-29 23:46:05 +0530

    Time: 43 ms (86.85%), Space: 29.1 MB (51.76%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,44 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
         sort(nums.begin(), nums.end());
-        // set<vector<int>> s;
-        vector<vector<int>> ans;
-
-        if(nums.size() < 3) {
-            return ans;
-        }
-        
         for(int i=0; i<nums.size(); i++) {
-            if(i>0 && nums[i] == nums[i-1]) {
-                continue;
-            }
-            int j = i+1;
-            int k = nums.size()-1;
+            if(i > 0 && nums[i]==nums[i-1]) continue;
+            int j = i+1, k = nums.size()-1;
             while(j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
+                int sum =nums[i]+nums[j]+nums[k];
                 if(sum == 0) {
-                    // s.insert({nums[i], nums[j], nums[k]});
-                    ans.push_back({nums[i], nums[j], nums[k]});
+                    res.push_back({nums[i], nums[j], nums[k]});
                     j++;
                     k--;
-                    while(j < k && nums[j]==nums[j-1]) {
-                        j++;
-                    } 
-                    while(j < k && nums[k] == nums[k+1]) {
-                        k--;
-                    }
-                } else if(sum > 0) {
-                    k--;
-                } else {
-                    j++;
-                }
+                    while(j<k && nums[k]==nums[k+1]) k--;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                } else if(sum > 0) k--;
+                else j++; 
             }
-            
         }
-        // for(auto triplets: s) {
-        //     ans.push_back(triplets);
-        // }
-        return ans;
+        return res;
     }
 };
